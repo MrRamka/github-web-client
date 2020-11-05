b0a307b0a1eaaf4250794e6efb760ac9abaefef7
Ramil <ramil.minyukov@yandex.ru>
2020-11-05 21:53:30 +0300

    Add login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,12 +1,68 @@
 import React, { FC } from 'react';
 import { CLIENT_ID, REDIRECT_URI } from '../../constants/oath';
+import {
+	BlueButtonOutline,
+	ButtonWrapper,
+	GithubLoginLogo,
+	GithubViewLogo,
+	InnerDiv,
+	LogoWrapper,
+	PageWrapper,
+	TextWrapper,
+	ViewCode,
+	ViewCodeBlock,
+	ViewCodeLink,
+	WelcomeText,
+	Wrapper
+} from './styles';
+import { useHistory } from 'react-router';
+import { Routes as R } from '../../constants';
 
 export const Login: FC = () => {
+
+	const url = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`;
+	const history = useHistory();
+
+
+	const onClick = () => {
+		history.push(url);
+	}
+
 	return (
-		<>
-			<h1>Hello man</h1>
-			<a href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}>
-				Login
-			</a>
-		</>);
+		<PageWrapper>
+			<Wrapper>
+				<LogoWrapper>
+					<GithubLoginLogo/>
+				</LogoWrapper>
+				<TextWrapper>
+					<InnerDiv>
+						<div>
+							<WelcomeText>
+								It is a long established fact that a reader will be distracted by the readable content
+								of a
+								<br/>
+								<br/>
+								page when looking at its layout. The point of using Lorem Ipsum is that it has a
+								more-or-less normal distribution of letters, as opposed to using 'Content here, content
+								here', making it look like readable English.
+							</WelcomeText>
+						</div>
+						<ButtonWrapper>
+							<BlueButtonOutline onClick={onClick}>
+								Login with GitHub
+							</BlueButtonOutline>
+						</ButtonWrapper>
+					</InnerDiv>
+				</TextWrapper>
+			</Wrapper>
+			<ViewCodeBlock>
+				<ViewCode>
+					<GithubViewLogo/>
+					&nbsp;&nbsp;
+					<ViewCodeLink href={R.REPO} target="_blank">View code</ViewCodeLink>
+				</ViewCode>
+			</ViewCodeBlock>
+
+		</PageWrapper>
+	);
 }
