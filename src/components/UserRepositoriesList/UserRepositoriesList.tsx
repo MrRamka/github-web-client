b1499bc227584405631ae638d500b103579027b2
easydush <easydush@gmail.com>
2020-11-16 00:10:06 +0300

    added viewer's repositories

---
+++ src/components/UserRepositoriesList/UserRepositoriesList.tsx
@@ -0,0 +1,26 @@
+import React, { FC, useEffect, useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { RepositoryNode } from '../../api/search';
+import { RepositoryList } from '../RepositoryList';
+import { UserRepositories } from '../../api/userRepositories';
+import { GET_USER_REPOSITORIES } from '../../api/userRepositories/queries';
+
+export const UserRepositoriesList: FC = () => {
+    const searchVariables = {
+        count: 10,
+    };
+    const {loading, data} = useQuery<UserRepositories>(GET_USER_REPOSITORIES, {
+        variables: {...searchVariables}
+    });
+    const [repositories, setRepositories] = useState<RepositoryNode[]>([]);
+
+    useEffect(() => {
+        if (!loading) {
+            setRepositories(data?.viewer.repositories.nodes ?? []);
+        }
+    }, [loading, data])
+
+    return (
+        <RepositoryList data={repositories} loading={loading} pageSize={4}/>
+    );
+}
