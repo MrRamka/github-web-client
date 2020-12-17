4a41cbdb45349fef5ad9c9d9081f64e82dfdff25
Ramil <ramil.minyukov@yandex.ru>
2020-12-18 02:20:10 +0300

    Updates

---
+++ precache-manifest.7bb726df1e751781970b7a0e8d886e8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d162aa8a58d84a972c1dced82b43fde1",
+    "url": "/github-web-client/index.html"
+  },
+  {
+    "revision": "f516d1f29527d9063104",
+    "url": "/github-web-client/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "22c80c4bf625f85842d8",
+    "url": "/github-web-client/static/css/main.67edbc49.chunk.css"
+  },
+  {
+    "revision": "f516d1f29527d9063104",
+    "url": "/github-web-client/static/js/2.3b80fc09.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/github-web-client/static/js/2.3b80fc09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c80c4bf625f85842d8",
+    "url": "/github-web-client/static/js/main.516fb5e5.chunk.js"
+  },
+  {
+    "revision": "d473570af28a2da5accc",
+    "url": "/github-web-client/static/js/runtime-main.33af6066.js"
+  }
+]);
